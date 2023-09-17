2342b0d5c5b46627ce782d55547bbe63e2cdc54f
TunelessBat <thiagoeletronicag7@gmail.com>
2023-09-17 18:32:55 -0300

    Primeira Classe Em C++

--- gradebook.cpp
+++ gradebook.cpp
@@ -2,10 +2,21 @@
 
 using namespace std;
 
+class GradeBook
+{
+    public:
+    //exibe mensagem na tela
+    void displayMessage()
+    {
+        cout << "Welcome to the Grade Book!" << endl;
+    }
+};
+
 int main()
 {
-    
-    
+    GradeBook myGradeBook;
+    myGradeBook.displayMessage();
+
     return 0;
 }
 
