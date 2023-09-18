a972fd50ad1ef1a07e8f1c7540a1b064a316a1d4
TunelessBat <thiagoeletronicag7@gmail.com>
2023-09-18 12:40:10 -0300

    adicionando parâmetros na função

--- gradebook.cpp
+++ gradebook.cpp
@@ -1,21 +1,32 @@
+//definindo a classe GradeBook com uma função que aceita parâmetro
+//cria um objeto GradeBook e chama sua função
 #include <iostream>
 
 using namespace std;
 
+#include <string>
+
+//definição da classe GradeBook
 class GradeBook
 {
     public:
-    //exibe mensagem na tela
-    void displayMessage()
+    //exibe mensagem de boas vindas
+    void displayMessage(string nomeDoCurso)
     {
-        cout << "Welcome to the Grade Book!" << endl;
+        cout << "Welcome to the Grade Book for\n" << nomeDoCurso << "!" << endl;
     }
 };
 
 int main()
 {
+    string nomeCurso;
     GradeBook myGradeBook;
-    myGradeBook.displayMessage();
+
+    cout << "Entre com o nome do curso:" << endl;
+    getline (cin, nomeCurso);
+    cout << endl;
+
+    myGradeBook.displayMessage(nomeCurso);
 
     return 0;
 }
